10db584a596fc48838bab34f6613f42852538dda
Franco Morales Castro <franco.morales@outlook.com>
2026-09-07 10:18:03 -0300

    feat: ejercicio (clase abstracta, herencia, polimorfismo y agregacion)

---
+++ poo_4/main.py
@@ -0,0 +1,5 @@
+# Generar una clase invetario que pueda recibir items o agregar items
+# donde contenga un metodo que muestre los que los items hacen.
+# Considerar una clase Item y sub-clases a partir de ella,
+# como por ejemplo: Arma, Pocion, Armadura, etc.
+
