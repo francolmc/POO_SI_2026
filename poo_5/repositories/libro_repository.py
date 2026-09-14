c65af008cf945b27ed54b448e50a586644b4eeb8
Franco Morales Castro <franco.morales@outlook.com>
2026-09-14 11:12:56 -0300

    metodos de actualizar y eliminar en libros

--- poo_5/repositories/libro_repository.py
+++ poo_5/repositories/libro_repository.py
@@ -12,9 +12,9 @@ class LibroRepository:
         )
         self.__conexion.commit()
 
-    def buscar_por_isbn(self, isbn:str) -> None | Libro:
+    def buscar_por_isbn(self, isbn:str):
         cursor = self.__conexion.cursor()
-        consulta = f"SELECT isbn, titulo, copias_diponibles FROM libros WHERE isbn = ?"
+        consulta = f"SELECT isbn, titulo, copias_disponibles FROM libros WHERE isbn = ?"
         cursor.execute(consulta, (isbn,))
         registros = cursor.fetchone()
         if registros is None:
@@ -25,7 +25,7 @@ class LibroRepository:
     def listar(self):
         cursor = self.__conexion.cursor()
         cursor.execute(
-            "SELECT isbn, titulo, copias_diponibles FROM libros"
+            "SELECT isbn, titulo, copias_disponibles FROM libros"
         )
         registros = cursor.fetchall() # lista de tuplas
         libros = []
@@ -34,3 +34,23 @@ class LibroRepository:
                 Libro(registro[0], registro[1], registro[2])
             )
         return libros
+
+    def actualizar(self, libro: Libro):
+        cursor = self.__conexion.cursor()
+        cursor.execute("""
+        UPDATE libros SET 
+            titulo = ?,
+            copias_disponibles = ?
+        WHERE
+            isbn = ?
+        """, (libro.titulo, libro.copias_disponibles, libro.isbn))
+        self.__conexion.commit()
+        return cursor.rowcount > 0
+
+    def eliminar(self, isbn: str):
+        cursor = self.__conexion.cursor()
+        cursor.execute("""
+        DELETE FROM libros WHERE isbn = ?
+        """, (isbn))
+        self.__conexion.commit()
+        return cursor.rowcount > 0
